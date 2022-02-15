4e57ec019e8b37c173786a5991bf9839ff1541f4
muzicos <daniel.phelan@colorado.edu>
2022-02-15 15:06:25 -0700

    Add files via upload

--- WF_Fluid_model.m
+++ WF_Fluid_model.m
@@ -3,10 +3,10 @@ clear all; close all; clc;
 
 %% Baseline variables
 filename = 'UVexp_Re22.17_dim_1.mat';
-N_d = 30;
-N_h = [3 3 3];
-D = [0.8, 140];
-track = true;
+N_d = 30;       % number of integration domains
+N_h = [3 3 3];  % [max harmonic in x,y resp. (>=0), max harmonic in time (>=1)]
+D = [0.8, 140]; % [spatial-area of int domain as fraction of full area, time-length of integration domain in grid points]
+track = false;  % boolean to track library filling in cmd window
 
-%% run weak formulation
+%% run weak formulation (Re = 22.17 -> mean values c1 = 0.889, c2 = 3.96e-6, c3 = 0.077)
 [ksi, res, Q, q0, P] = ParEst_WF_Kolmo(filename, N_d, N_h, D, track);
